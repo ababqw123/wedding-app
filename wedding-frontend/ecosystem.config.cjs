1f686c9a4332dbbcaa0a60c54173862e35f1352c
JaeYeongAn <96455920+ababqw123@users.noreply.github.com>
2024-04-09 14:11:53 +0900

    파일 변경

--- wedding-frontend/ecosystem.config.cjs
+++ wedding-frontend/ecosystem.config.cjs
@@ -3,12 +3,12 @@ module.exports = {
     {
       // 운영 환경변수 (yarn pm2start)
       name: "wedding-frontend",
-      script: "./server.js",
+      script: "node_modules/next/dist/bin/next",
+      args: "start -p 80",
       instances: 4,
       exec_mode: "cluster",
       max_memory_restart: "500M",
       env: {
-        PORT: 80,
         MODE: "production",
       },
     },

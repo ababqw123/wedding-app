a6066c820ebaf5bbfc66a3cbd13a2383d3b03ad5
안재영 <ababqw123@gmail.com>
2024-04-25 16:25:33 +0900

    다시 살림

--- wedding-frontend/next.config.js
+++ wedding-frontend/next.config.js
@@ -2,9 +2,9 @@
 const runtimeCaching = require("next-pwa/cache");
 const withPWA = require("next-pwa")({
   dest: "public",
-  // register: true,
-  // skipWaiting: true,
-  // runtimeCaching,
+  register: true,
+  skipWaiting: true,
+  runtimeCaching,
 });
 
 const nextConfig = withPWA({

29f52d6bec717f8b9f84a86f11f099b46a680031
JaeYeongAn <96455920+ababqw123@users.noreply.github.com>
2024-04-25 09:46:01 +0900

    Modify: 수정
    - 수정

--- wedding-backend/src/main.ts
+++ wedding-backend/src/main.ts
@@ -3,12 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'https://wedding-app-delta.vercel.app',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-  });
+  app.enableCors();
   await app.listen(8002);
 }
 bootstrap();
